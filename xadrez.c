7372df954c4f17149db62bdd43d1deadc8e169b7
Hugo Facini <news.word.game@gmail.com>
2025-03-28 09:15:09 -0300

    Conceito de movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -7,25 +7,37 @@ int main() {
     int movRainha = 8; 
 
     // Exibindo os movimentos de cada peça
-    printf("**Movimentos da Torre**\n");
+    printf("** Movimentos da Torre **\n");
 
     for (int i = 0; i < movTorre; i++) {
         printf("Direita\n");
     }
 
-    printf("\n**Movimentos do Bispo**\n");
+    printf("\n** Movimentos do Bispo **\n");
     
     while (movBispo > 0) {
         printf("Cima direita\n");
         movBispo--;
     }
 
-    printf("\n**Movimentos da Rainha**\n");
+    printf("\n** Movimentos da Rainha **\n");
 
     do {
         printf("Esquerda\n");
         movRainha--;
     } while (movRainha > 0);
+
+	// Movimentos cavalo
+	int j = 0;
+	printf("\n** Movimentos do Cavalo **\n");
+
+	for (int i = 0; i < 1; i++) {
+		while (j < 2) {
+			printf("Baixo\n");
+			j++;
+		}
+		printf("Esquerda\n");
+	}
 
     return 0;
 }
